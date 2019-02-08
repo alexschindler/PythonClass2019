df2c78f5a860acfa175b5715bcd15fe0ce19be0c
jakobrichi <jakob.richi@uzh.ch>
2019-02-08 11:05:27 +0100

    added Birthdate Picker

--- Customer_Map_E3.py
+++ Customer_Map_E3.py
@@ -53,6 +53,19 @@ app.layout = html.Div([html.H1('Customer Map', style={'textAlign':'center'}),
                                                                     start_date=min(demographics.JoinDate),
                                                                     end_date=max(demographics.JoinDate)
                                                                     )
+                                                            
+                                                            ),
+                                                    html.H6('Birth Date'),
+                                                    html.P(
+                                                            dcc.DatePickerRange(
+                                                                    id='birthdate',
+                                                                    min_date_allowed=min(demographics.Birthdate),
+                                                                    max_date_allowed=max(demographics.Birthdate),
+                                                                    initial_visible_month=dt(1989, 11, 9),
+                                                                    start_date=min(demographics.Birthdate),
+                                                                    end_date=max(demographics.Birthdate)
+                                                                    )
+                                                            
                                                             )
                                                     ])
                                             ],
@@ -83,12 +96,16 @@ app.layout = html.Div([html.H1('Customer Map', style={'textAlign':'center'}),
     dash.dependencies.Output('CustomerMap', 'figure'),
     [dash.dependencies.Input('gender-picker', 'values'),
      dash.dependencies.Input('date-picker-range', 'start_date'),
-     dash.dependencies.Input('date-picker-range', 'end_date')])
+     dash.dependencies.Input('date-picker-range', 'end_date'),
+     dash.dependencies.Input('birthdate', 'start_date'),
+     dash.dependencies.Input('birthdate', 'end_date')])
 
-def update_figure(selected_gender, start_date, end_date):    
+def update_figure(selected_gender, start_date, end_date, birthdate_start_date, birthdate_end_date):    
      filtered_df = demographics.loc[(demographics['Gender'].isin(selected_gender)) &  
                                   (demographics['JoinDate'] >= start_date) &
-                                  (demographics['JoinDate'] <= end_date) ,]
+                                  (demographics['JoinDate'] <= end_date) &
+                                  (demographics['Birthdate'] >= birthdate_start_date) &
+                                  (demographics['Birthdate'] <= birthdate_end_date), ]
     
      zip_size = filtered_df.groupby(["zip_city", 'zip_longitude', 'zip_latitude']).size()
     
@@ -122,12 +139,16 @@ def update_figure(selected_gender, start_date, end_date):
     dash.dependencies.Output('table', 'data'),
     [dash.dependencies.Input('gender-picker', 'values'),
      dash.dependencies.Input('date-picker-range', 'start_date'),
-     dash.dependencies.Input('date-picker-range', 'end_date')])
+     dash.dependencies.Input('date-picker-range', 'end_date'),
+     dash.dependencies.Input('birthdate', 'start_date'),
+     dash.dependencies.Input('birthdate', 'end_date')])
 
-def update_table(selected_gender, start_date, end_date):    
+def update_table(selected_gender, start_date, end_date, birthdate_start_date, birthdate_end_date):    
     filtered_df = demographics.loc[(demographics['Gender'].isin(selected_gender)) &  
                                   (demographics['JoinDate'] >= start_date) &
-                                  (demographics['JoinDate'] <= end_date), ]
+                                  (demographics['JoinDate'] <= end_date) &
+                                  (demographics['Birthdate'] >= birthdate_start_date) &
+                                  (demographics['Birthdate'] <= birthdate_end_date), ]
     return filtered_df.to_dict("rows")
 
 if __name__ == '__main__':
